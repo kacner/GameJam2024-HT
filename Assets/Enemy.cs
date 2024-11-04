74e0e56bf52df1622dfaf99994ca69178a375677
ItsUrBoyDhito <122612416+ItsUrBoyDhito@users.noreply.github.com>
2024-11-04 14:57:51 +0100

    etdryuio

---
+++ Assets/Enemy.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject Wall;
+    public float speed;
+
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, Wall.transform.position);
+        Vector2 direction = (Wall.transform.position - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.position = Vector2.MoveTowards(this.transform.position, Wall.transform.position, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+    }
+}
